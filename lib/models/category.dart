be039185ac215e9417ccedc9811f91ce6bd44188
Rishabh Varshney <right.rishabh@gmail.com>
2024-09-20 12:21:12 +0530

    Category and user model updated

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,7 +1,15 @@
+import 'dart:convert';
+
+ProductCategory productCategoryFromJson(String str) =>
+    ProductCategory.fromJson(json.decode(str));
+
+String productCategoryToJson(ProductCategory data) =>
+    json.encode(data.toJson());
+
 class ProductCategory {
-  final int id;
-  final String name;
-  final String image;
+  int id;
+  String name;
+  String image;
 
   ProductCategory({
     required this.id,
@@ -9,11 +17,16 @@ class ProductCategory {
     required this.image,
   });
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'image': image,
-    };
-  }
+  factory ProductCategory.fromJson(Map<String, dynamic> json) =>
+      ProductCategory(
+        id: json["id"],
+        name: json["name"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "image": image,
+      };
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+
+String userToJson(User data) => json.encode(data.toJson());
+
 class User {
   String name;
   String email;
@@ -11,21 +17,17 @@ class User {
     required this.avatar,
   });
 
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'email': email,
-      'password': password,
-      'avatar': avatar,
-    };
-  }
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        name: json["name"],
+        email: json["email"],
+        password: json["password"],
+        avatar: json["avatar"],
+      );
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      name: json['name'],
-      email: json['email'],
-      password: json['password'],
-      avatar: json['avatar'],
-    );
-  }
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "email": email,
+        "password": password,
+        "avatar": avatar,
+      };
 }
